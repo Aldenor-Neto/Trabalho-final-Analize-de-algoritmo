b0975c99e353797c6137e3665d7fb3fc7d312d81
aldenor-neto <aldenor05062010@gmail.com>
2024-08-04 12:17:48 -0300

    insert array cuts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,15 +25,16 @@ public class Main {
     }
 
     // Função para calcular o lucro máximo
-    public static int rodCutting(int[] prices, int n) {
-        // Array para armazenar os lucros máximos para cada comprimento
+    public static int rodCutting(int[] prices, int n, int[] cuts) {
         int[] array = new int[n + 1];
 
-        // Loop para calcular o lucro máximo para cada comprimento
         for (int i = 1; i <= n; i++) {
             int maxValue = Integer.MIN_VALUE;
             for (int j = 1; j <= i; j++) {
-                maxValue = Math.max(maxValue, prices[j - 1] + array[i - j]);
+                if (maxValue < prices[j - 1] + array[i - j]) {
+                    maxValue = prices[j - 1] + array[i - j];
+                    cuts[i] = j;
+                }
             }
             array[i] = maxValue;
         }
@@ -42,13 +43,13 @@ public class Main {
     }
 
     // Função para calcular o lucro máximo recursivamente com memorização
-    public static int rodCuttingRecursive(int[] prices, int n) {
+    public static int rodCuttingRecursive(int[] prices, int n, int[] cuts) {
         int[] memo = new int[n + 1];
         Arrays.fill(memo, -1);
-        return rodCuttingRecursiveAux(prices, n, memo);
+        return rodCuttingRecursiveAux(prices, n, memo, cuts);
     }
 
-    public static int rodCuttingRecursiveAux(int[] prices, int n, int[] memo) {
+    public static int rodCuttingRecursiveAux(int[] prices, int n, int[] memo, int[] cuts) {
         if (n == 0) {
             return 0;
         }
@@ -59,18 +60,34 @@ public class Main {
 
         int maxValue = Integer.MIN_VALUE;
         for (int i = 1; i <= n; i++) {
-            maxValue = Math.max(maxValue, prices[i - 1] + rodCuttingRecursiveAux(prices, n - i, memo));
+            int tempValue = prices[i - 1] + rodCuttingRecursiveAux(prices, n - i, memo, cuts);
+            if (maxValue < tempValue) {
+                maxValue = tempValue;
+                cuts[n] = i;
+            }
         }
 
         memo[n] = maxValue;
         return maxValue;
     }
 
+    public static void printCuts(int[] cuts, int n) {
+        System.out.print("Cortes: ");
+        while (n > 0) {
+            System.out.print(cuts[n] + " ");
+            n -= cuts[n];
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
-        // Exemplo de uso
-        int[] bars = {1, 50, 250, 1000, 15000};
-        int[] prices = generateSortedArray(bars[bars.length - 1]);
+        //int[] bars = {1, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000};
+        //int[] prices = generateSortedArray(bars[bars.length - 1]);
+
+        int[] bars = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] prices = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
+        int[] cuts = new int[bars.length + 1];
 
         Long[] tempIterative = new Long[bars.length];
         int[] maxValueIterative = new int[bars.length];
@@ -85,12 +102,12 @@ public class Main {
 
             for (int j = 0; j < 5; j++) {
                 Long startIterative = System.nanoTime();
-                maxValueIterative[i] = rodCutting(prices, bars[i]);
+                maxValueIterative[i] = rodCutting(prices, bars[i], cuts);
                 Long endIterative = System.nanoTime();
                 mediaTempIterative += endIterative - startIterative;
 
                 Long startRecursive = System.nanoTime();
-                maxValueRecursive[i] = rodCuttingRecursive(prices, bars[i]);
+                maxValueRecursive[i] = rodCuttingRecursive(prices, bars[i], cuts);
                 Long endRecursive = System.nanoTime();
                 mediaTempRecursive += endRecursive - startRecursive;
             }
@@ -103,6 +120,8 @@ public class Main {
             System.out.println("Lucro maxímo: " + maxValueIterative[i]);
             System.out.println("Tempo iterativo: " + tempIterative[i]);
             System.out.println("Tempo recursivo: " + tempRecursive[i]);
+
+            printCuts(cuts, bars[i]);
             System.out.println();
         }
 
